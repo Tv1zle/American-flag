b65a9f2d9c0d964de33267f16733656727076219
User <User@WIN-D6I7BGDFHRA>
2022-06-15 19:07:36 +0200

    Добавьте файлы проекта.

---
+++ HomeWork 14.cpp
@@ -0,0 +1,67 @@
+﻿#include <iostream>
+#include <windows.h>
+
+void color(int text, int background)
+{
+	HANDLE h = GetStdHandle(STD_OUTPUT_HANDLE);
+	SetConsoleTextAttribute(h, (WORD)((background << 4) | text));
+}
+
+int main()
+{
+	system("color 07");
+
+
+	int width = 26; 
+	int height = 13, count = 0;
+
+
+	for (int y = 0; y < height; y++)
+	{
+		for (int x = 0; x < width; x++)
+		{
+			if (x < width / 2.5 && y < height / 2 + 1)
+			{
+				color(15, 1);
+				if (y % 2 != 0)
+				{
+					if (x % 2 != 0)
+					{
+						std::cout << "*";
+						count++;
+					}
+				}
+				else
+				{
+					if (x % 2 == 0)
+					{
+						std::cout << "*";
+						count++;
+					}
+				}
+			}
+			else if (y % 2 == 0)
+			{
+				color(15, 12);
+			}
+			else
+			{
+				color(15, 15);
+			}
+
+			if (count == 0)
+			{
+				std::cout << " " << " ";
+			}
+			else
+			{
+				std::cout << " ";
+			}
+			count = 0;
+		}
+		std::cout << "\n";
+	}
+	color(15, 0);
+
+
+}
